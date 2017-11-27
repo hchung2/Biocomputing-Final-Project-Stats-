f4607f6c145fbe5477d3dc85c6b1990e1592e33f
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2017-11-27 11:19:53 -0500

    Initial stages of PseudoCode for regression

---
+++ PseudoCodeSugarRegression.R
@@ -0,0 +1,8 @@
+#Load csv file
+
+#Plot the data from the csv - probably ought to be a scatterplot
+
+#Make a regression design linear model
+  #Make a neg-log-like function and fit it to a linear regression model using appropriate data where appropriate
+
+#Write up stuff in markdown
