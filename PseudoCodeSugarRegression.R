d362541c1c1ca5510c17042b754bb6cc4b9c96af
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2017-11-27 17:48:29 -0500

    Updated PseudoCode so it proscribes the correct test

--- PseudoCodeSugarRegression.R
+++ PseudoCodeSugarRegression.R
@@ -8,12 +8,13 @@
 # - dnorm(x,mean,sd)
 
 #Asses max likelihood of model parameters with optim()
-# - create vector with initial guess
+#   Create one function to fit a constant, and one to fit a linear model
+# - create vector with initial guess for each model
 #       -maybe look at data to guide guesses, because these models seem quite chaotic
-# - minimization function with appropriate arguments
+# - minimization function with appropriate arguments - use optim - store each model seperately
 
 #Implement the liklihood ratio test
 #   pchisq(D,df=1)
-#   D = 2times difference of negative log-likelihood
+#   D = 2times difference of negative log-likelihood - these two values come from the different models
 
 #Print the p-value
