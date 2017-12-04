dd53ece33006d9173508019fcbe190532bf3a976
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2017-12-04 17:37:38 -0500

    Pseudocode for Antibiotic ANOVA analysis.  Subject to change

---
+++ PseudoCodeAntibioticsANOVA.R
@@ -0,0 +1,24 @@
+#Load csv file
+
+#Modify data so it works with ANOVA test - we live in R^4 I think
+#Model is a function f:R^3->R, so we need to take three vectors as inputs
+#Thus data should be a 3 by 5 matrix?
+
+#Make a custom function(params, observation)
+# - unload parameters
+# - Match parameters to ANOVA model - B0+B1x1+B2x2+B3x3+error
+#control mean=B0, ab1 mean=B1+B0, ab2 mean=B2+B0, ab3 mean=B3+B0
+# - dnorm(x,mean,sd)
+# - repeat process for null model - B0+error
+
+#Asses max likelihood of model parameters with optim()
+#   Create one function to fit a constant, and one to fit a linear model
+# - create vector with initial guess for each model
+#       -maybe look at data to guide guesses, because these models seem quite chaotic
+# - minimization function with appropriate arguments - use optim - store each model seperately
+
+#Implement the liklihood ratio test
+#   pchisq(D,df=1)
+#   D = 2times difference of negative log-likelihood - these two values come from the different models
+
+#Print the p-value
